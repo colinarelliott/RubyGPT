61da3d04f64fc5e5e16a0a8c01024eae8e1cee8a
Colin Elliott <colinarelliott@gmail.com>
2023-03-13 01:43:48 -0400

    adjusted temperature

--- chatgpt.rb
+++ chatgpt.rb
@@ -23,7 +23,7 @@ def gpt(prompt, client)
     parameters: {
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: prompt}],
-        temperature: 0.7,
+        temperature: 0.9,
     })
     return response
 end
